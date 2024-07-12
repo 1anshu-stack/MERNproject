f3697ee9a1f3c47ca7f50ed0aba1fb20f0ba5e31
1anshu-stack <71838931+1anshu-stack@users.noreply.github.com>
2024-07-12 13:27:10 +0000

    poy

---
+++ mongoose2.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+const uri = 'mongodb+srv://jogi661:@N$hu-11228@cluster0.7p6vf67.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(uri);
+
+const bookSchema = new mongoose.Schema({
+    title: String,
+    author: String
+})
+const Books = new mongoose.model('Book', bookSchema);
+
+app.get('/books', async (req, res)=>{
+    try{
+        const books = await Books.find();
+        res.json(books);
+    }
+    catch(err){
+        res.status(500).json({erro: 'some external error' })
+    }
+})
+
+app.post('/books', async (req, res)=>{
+    console.log(req.body);
+    try{
+        const {title, author} = req.body;
+        const newBook = new Books({title, author}); 
+        await newBook.save(); 
+        res.status(201).json(newBook);
+    }
+    catch(err){
+        res.status(404).json({err: 'some internal error'})
+    }
+})
+
+app.put('/books/:id', async (req, res)=> {
+    console.log(req.params);
+    const {id} = req.params;
+    const {title, author} = req.params;
+    const updatedbook = await Books.findByIDAndUpdate(id, {title, author});
+    req.json(updatedbook);
+})
+
+app.delete('/books/:id', async (req, res)=> {
+    const {id} = req.params;
+    await Books.findByIDAndRemove(id);
+    req.sendStatus(204);
+})
+
+app.listen(3000, ()=>{
+    console.log('server is created...')
+})
